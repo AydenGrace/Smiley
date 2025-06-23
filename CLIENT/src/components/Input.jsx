328d11bc624184d353ecf49efab8edd9af4a89e1
Pierre-Alexandre <161696548+AydenGrace@users.noreply.github.com>
2025-06-23 15:11:17 +0200

    Contact

--- CLIENT/src/components/Input.jsx
+++ CLIENT/src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useMemo, useState} from "react";
 import Tooltip from "./Tooltip";
 import {FaRegEye, FaRegEyeSlash} from "react-icons/fa6";
 
@@ -7,43 +7,58 @@ export default function Input({
   type = "text",
   hasTooltip = false,
   placeholder = "",
+  isTextArea = false,
+  rows = 5,
   getValueOnChange = () => {},
 }) {
   const [value, setValue] = useState("");
   const [localType, setlocalType] = useState(type);
+  const RANDOM = Math.floor(Math.random() * 10000);
 
-  const handleChange = (e) => {
-    getValueOnChange(e.target.value);
-  };
+  useEffect(() => {
+    getValueOnChange(value);
+  }, [value]);
   return (
     <div className="flex flex-col gap-1 w-full">
       {label && (
         <div className="flex items-center gap-2">
-          <p className="font-semibold">{label}</p>
+          <label htmlFor={`${type}_${RANDOM}`} className="font-semibold">
+            {label}
+          </label>
           {hasTooltip && <Tooltip textToVerify={value} />}
         </div>
       )}
-      <div className="relative">
-        <input
-          type={localType}
-          value={value}
-          className={`w-full`}
+      {isTextArea ? (
+        <textarea
+          id={`${type}_${RANDOM}`}
+          className="min-h-[70px]"
           placeholder={placeholder}
-          onChange={(e) => setValue(e.target.value)}
-        />
-        {type === "password" && (
-          <div className="absolute top-3.5 right-4 cursor-pointer text-primary">
-            {localType === "password" ? (
-              <FaRegEye size={18} onClick={() => setlocalType("text")} />
-            ) : (
-              <FaRegEyeSlash
-                size={18}
-                onClick={() => setlocalType("password")}
-              />
-            )}
-          </div>
-        )}
-      </div>
+          rows={rows}
+        ></textarea>
+      ) : (
+        <div className="relative">
+          <input
+            id={`${type}_${RANDOM}`}
+            type={localType}
+            value={value}
+            className={`w-full`}
+            placeholder={placeholder}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          {type === "password" && (
+            <div className="absolute top-3.5 right-4 cursor-pointer text-primary">
+              {localType === "password" ? (
+                <FaRegEye size={18} onClick={() => setlocalType("text")} />
+              ) : (
+                <FaRegEyeSlash
+                  size={18}
+                  onClick={() => setlocalType("password")}
+                />
+              )}
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
--- CLIENT/src/pages/Contact.jsx
+++ CLIENT/src/pages/Contact.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Title from "../components/Title";
 import SubTitle from "../components/SubTitle";
 import Input from "../components/Input";
+import {Link} from "react-router-dom";
+import Button from "../components/Button";
 
 export default function Contact() {
   return (
     <div className="w-full min-h-screen pt-[70px] flex items-center justify-center">
-      <div className="w-1/2 flex flex-col justify-center items-center text-center gap-20">
+      <div className="px-4 w-full md:w-3/4 lg:w-1/2 flex flex-col justify-center items-center text-center gap-20">
         <div className="flex flex-col gap-6">
           <Title>
             Restons en <span className="text-primary font-medium">Contact</span>
@@ -17,11 +19,45 @@ export default function Contact() {
             utiliser le formulaire ci-dessous !
           </SubTitle>
         </div>
-        <form className="w-full flex flex-col">
-          <div className="w-full flex">
+        <form className="w-full flex flex-col gap-4">
+          <div className="w-full flex gap-4 flex-col sm:flex-row">
             <div className="w-full max-w-[640px]">
-              <Input label={"Mot de passe"} type="text" />
+              <Input label={"Votre nom"} type="text" placeholder="John Doe" />
             </div>
+            <div className="w-full max-w-[640px]">
+              <Input
+                label={"Votre email"}
+                type="text"
+                placeholder="john.doe@gmail.com"
+              />
+            </div>
+          </div>
+          <Input
+            label={"Objet du message"}
+            type="text"
+            placeholder="Envoi de mots doux"
+          />
+          <Input
+            label={"Message"}
+            isTextArea
+            placeholder="Vous qui lisez ce message, vous êtes magnifique ! ... J’ai vu votre sourire !"
+          />
+          <div className="w-full flex gap-2 items-center text-left">
+            <input
+              type="checkbox"
+              id="contact_rgpd"
+              className="!rounded-[5px] mt-1 relative peer appearance-none min-w-4 max-w-4 maw-h-4 min-h-4 border border-primary cursor-pointer text-bg checked:bg-secondary checked:!bg-primary before:checked:content-[2714] before:checked:absolute before:checked:right-[1px] before:checked:top-[-5px] before:checked:text-primary check"
+            />
+            <label htmlFor="contact_rgpd" className="pt-1">
+              En continuant, vous validez avoir lu et accepté les 
+              <Link className="underline text-primary">
+                politiques de confidentialité
+              </Link>
+              .
+            </label>
+          </div>
+          <div className="w-full flex items-center justify-center">
+            <Button text="Envoyer mon message" isFull />
           </div>
         </form>
       </div>
